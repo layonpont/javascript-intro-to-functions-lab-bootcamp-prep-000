a14d80ca3a6ad546414c752d1b47eb8d876b3a8e
dashing-page-6640 <elhadi.sulton@gmail.com>
2018-09-27 21:24:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,11 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toLowerCase){
     return "I can\'t hear you!";
-  } 
+  }
+}
+
+function sayHiToGrandma(string){
+  if(string.toUpperCase){
+     return "YES INDEED!";
+  }
 }
